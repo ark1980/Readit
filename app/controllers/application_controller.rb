11143c8812e8c6002ef04017209ba20d03b43d7d
Ali <ar.k@outlook.com>
2015-07-07 12:22:58 -0700

    Assignment 37 completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :flash_attack
+
+  protected
  
-   protected
- 
-   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) << :name
-   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def flash_attack
+    flash[:notice] = "This is a test for assignment 37."
+  end
 end
