1830f85fcb1736184ab978278a6ececa24b9e4c4
Ali <ar.k@outlook.com>
2015-08-14 22:37:29 -0700

    Modified topics controller and views to show the pagination

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,13 @@
 class TopicsController < ApplicationController
 
   def index
-    @topics = Topic.all
+    @topics = Topic.all.paginate(page: params[:page], per_page: 10)
     authorize @topics
   end
 
   def show
     @topic = Topic.find(params[:id])
-    @posts = @topic.posts
+    @posts = @topic.posts.paginate(page: params[:page], per_page: 5)
     authorize @topic
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,78 @@
 require 'faker'
 
 # Create Users
- 5.times do
-   user = User.new(
-     name:     Faker::Name.name,
-     email:    Faker::Internet.email,
-     password: Faker::Lorem.characters(10)
-   )
-   user.skip_confirmation!
-   user.save!
- end
- users = User.all
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
 
 
- # Create Posts
- 50.times do
-   Post.create!(
+# Create Topics
+15.times do
+  Topic.create(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
+# Create Posts
+50.times do
+  post = Post.create(
     user:   users.sample,
+    topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  comment = Comment.create(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+)
+member.skip_confirmation!
+member.save
 
 
- # Create Comments
- 100.times do
-   Comment.create!(
-    # user: users.sample,   # we have not yet associated Users with Comments
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
-
-
-  # Create an admin user
- admin = User.new(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
- admin.skip_confirmation!
- admin.save!
-
-
- # Create a moderator
- moderator = User.new(
-   name:     'Moderator User',
-   email:    'moderator@example.com',
-   password: 'helloworld',
-   role:     'moderator'
- )
- moderator.skip_confirmation!
- moderator.save!
-
-
- # Create a member
- member = User.new(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
- member.skip_confirmation!
- member.save!
-
- puts "Seed finished"
- puts "#{User.count} users created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{User.count} Users created"
+puts "#{Topic.count} Topics created"
+puts "#{Post.count} Posts created"
+puts "#{Comment.count} Comments created"
