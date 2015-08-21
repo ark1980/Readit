93dcaf2cfe5838aaae5618847c1ab3a8603d279f
Ali <ar.k@outlook.com>
2015-08-21 12:12:32 -0700

    Add policy to comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment].permit(:body))
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Your comment has saved"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the comment. Please try again"
+      render :new
+    end
+  end
+
+end
