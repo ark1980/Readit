f30d51b04429598ea8e4366d6930f05de3a7274b
Ali <ar.k@outlook.com>
2015-08-30 19:15:18 -0700

    Create vote_spec file

--- app/controllers/advertisments_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdvertismentsController < ApplicationController
-  def index
-    @advertisments = Advertisment.all
-  end
-
-  def show
-    @advertisment = Advertisment.find(params[:id])
-  end
-end
--- app/models/advertisment.rb
+++
@@ -1,2 +0,0 @@
-class Advertisment < ActiveRecord::Base
-end
